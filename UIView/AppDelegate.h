8812f906f11a2bced23a156b7ed2d2cf4f9befc0
pumpkin <zhenhuanlee@outlook.com>
2017-06-21 15:18:05 +0800

    Initial Commit

---
+++ UIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView
+//
+//  Created by Aico on 6/21/17.
+//  Copyright © 2017 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView
+//
+//  Created by Aico on 6/21/17.
+//  Copyright © 2017 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
